62c952a4afb8a4a77e834f6483907be11c61bf66
Ron-Ashkenazi <ron98002@gmail.com>
2023-03-13 15:11:01 +0200

    change

--- Ron.java
+++ Ron.java
@@ -1,7 +1,7 @@
 public class Ron {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            System.out.println("Ron " + i);
+            System.out.println("Ron Ron " + i);
         }
     }
 }
